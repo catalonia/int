867cb0a82d03b534a04acd8a4fd45415fd18c7af
dusseltal <jagmohan.khulbe@gmail.com>
2013-09-16 22:19:46 +0200

    algo 2 online result

---
+++ algo/src/com/tastesync/algo/user/restaurant/RestaurantsSearchResultsOnlineCalc.java
@@ -0,0 +1,29 @@
+package com.tastesync.algo.user.restaurant;
+
+import com.tastesync.algo.db.dao.UserRestaurantDAO;
+import com.tastesync.algo.db.dao.UserRestaurantDAOImpl;
+import com.tastesync.algo.exception.TasteSyncException;
+
+import java.util.List;
+
+
+public class RestaurantsSearchResultsOnlineCalc {
+    private UserRestaurantDAO userRestaurantDAO = new UserRestaurantDAOImpl();
+
+    // return list of restaurantIds based on different parameters
+    public List<String> showListOfRestaurantsSearchResults(String userId,
+        String restaurantId, String neighborhoodId, String cityId,
+        String stateName, String[] cuisineTier1IdArray, String[] priceIdList,
+        String rating, String savedFlag, String favFlag, String dealFlag,
+        String chainFlag, String paginationId, String[] cuisineTier2IdArray,
+        String[] themeIdArray, String[] whoareyouwithIdArray,
+        String[] typeOfRestaurantIdArray, String[] occasionIdArray)
+        throws TasteSyncException {
+        return userRestaurantDAO.showListOfRestaurantsSearchResults(userId,
+            restaurantId, neighborhoodId, cityId, stateName,
+            cuisineTier1IdArray, priceIdList, rating, savedFlag, favFlag,
+            dealFlag, chainFlag, paginationId, cuisineTier2IdArray,
+            themeIdArray, whoareyouwithIdArray, typeOfRestaurantIdArray,
+            occasionIdArray);
+    }
+}
