d3e4ea8e8988268d034127013141c8b1c4314738
dusseltal <jagmohan.khulbe@gmail.com>
2013-09-24 22:49:20 +0200

    restaurant search results -order

--- algo/src/com/tastesync/algo/user/restaurant/RestaurantsSearchResultsOnlineCalc.java
+++ algo/src/com/tastesync/algo/user/restaurant/RestaurantsSearchResultsOnlineCalc.java
@@ -3,26 +3,25 @@ package com.tastesync.algo.user.restaurant;
 import com.tastesync.algo.db.dao.UserRestaurantDAO;
 import com.tastesync.algo.db.dao.UserRestaurantDAOImpl;
 import com.tastesync.algo.exception.TasteSyncException;
-
-import java.util.List;
+import com.tastesync.algo.model.vo.RestaurantsSearchResultsVO;
 
 
 public class RestaurantsSearchResultsOnlineCalc {
     private UserRestaurantDAO userRestaurantDAO = new UserRestaurantDAOImpl();
 
     public RestaurantsSearchResultsOnlineCalc() {
-		super();
-	}
+        super();
+    }
 
-	// return list of restaurantIds based on different parameters
-    public List<String> showListOfRestaurantsSearchResults(String userId,
-        String restaurantId, String neighborhoodId, String cityId,
-        String stateName, String[] cuisineTier1IdArray, String[] priceIdList,
-        String rating, String savedFlag, String favFlag, String dealFlag,
-        String chainFlag, String paginationId, String[] cuisineTier2IdArray,
-        String[] themeIdArray, String[] whoareyouwithIdArray,
-        String[] typeOfRestaurantIdArray, String[] occasionIdArray)
-        throws TasteSyncException {
+    // return list of restaurantIds based on different parameters
+    public RestaurantsSearchResultsVO showListOfRestaurantsSearchResults(
+        String userId, String restaurantId, String neighborhoodId,
+        String cityId, String stateName, String[] cuisineTier1IdArray,
+        String[] priceIdList, String rating, String savedFlag, String favFlag,
+        String dealFlag, String chainFlag, String paginationId,
+        String[] cuisineTier2IdArray, String[] themeIdArray,
+        String[] whoareyouwithIdArray, String[] typeOfRestaurantIdArray,
+        String[] occasionIdArray) throws TasteSyncException {
         return userRestaurantDAO.showListOfRestaurantsSearchResults(userId,
             restaurantId, neighborhoodId, cityId, stateName,
             cuisineTier1IdArray, priceIdList, rating, savedFlag, favFlag,
