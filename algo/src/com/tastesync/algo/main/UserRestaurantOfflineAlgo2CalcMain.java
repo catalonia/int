243dba89b53f5ca08c95655344569fa6b2016d6d
dusseltal <jagmohan.khulbe@gmail.com>
2013-10-03 23:19:47 +0200

    algo 1 / algo 2 main

---
+++ algo/src/com/tastesync/algo/main/UserRestaurantOfflineAlgo2CalcMain.java
@@ -0,0 +1,60 @@
+package com.tastesync.algo.main;
+
+import com.tastesync.algo.exception.TasteSyncException;
+import com.tastesync.algo.user.restaurant.RestInfoPopularityTierCalc;
+import com.tastesync.algo.user.restaurant.RestUserMatchCounterCalc;
+import com.tastesync.algo.user.restaurant.UserRestRankOrderCalc;
+
+import com.tastesync.db.pool.TSDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class UserRestaurantOfflineAlgo2CalcMain {
+    public static void main(String[] args) {
+        TSDataSource tsDataSource = null;
+        Connection connection = null;
+
+        try {
+            //Get DB connection object
+            tsDataSource = TSDataSource.getInstance();
+
+            connection = tsDataSource.getConnection();
+
+            tsDataSource.begin();
+
+            //Run algo calc
+            RestInfoPopularityTierCalc restInfoPopularityTierCalc = new RestInfoPopularityTierCalc();
+            restInfoPopularityTierCalc.processAllFlaggedRestaurantListRestInfoPopularityTier();
+            tsDataSource.commit();
+            tsDataSource.begin();
+
+            RestUserMatchCounterCalc restUserMatchCounterCalc = new RestUserMatchCounterCalc();
+            restUserMatchCounterCalc.processAllFlaggedRestaurantListRestUserMatchCounter();
+            tsDataSource.commit();
+            tsDataSource.begin();
+
+            UserRestRankOrderCalc userRestRankOrderCalc = new UserRestRankOrderCalc();
+            userRestRankOrderCalc.updateUserRestRankOrderCalc();
+            tsDataSource.commit();
+        } catch (TasteSyncException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            if (tsDataSource != null) {
+                try {
+                    tsDataSource.rollback();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        } finally {
+            if (tsDataSource != null) {
+                tsDataSource.close();
+                tsDataSource.closeConnection(connection, null, null);
+            }
+        }
+    }
+}
---
+++ algo/src/com/tastesync/algo/main/UserUserOfflineAlgo1CalcMain.java
@@ -0,0 +1,65 @@
+package com.tastesync.algo.main;
+
+import com.tastesync.algo.exception.TasteSyncException;
+import com.tastesync.algo.user.user.DemandPriorityCalc;
+import com.tastesync.algo.user.user.SupplyInventoryCalc;
+import com.tastesync.algo.user.user.UserTopicCalc;
+import com.tastesync.algo.user.user.UserUserCalc;
+
+import com.tastesync.db.pool.TSDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class UserUserOfflineAlgo1CalcMain {
+    public static void main(String[] args) {
+        TSDataSource tsDataSource = null;
+        Connection connection = null;
+
+        try {
+            //Get DB connection object
+            tsDataSource = TSDataSource.getInstance();
+
+            connection = tsDataSource.getConnection();
+
+            tsDataSource.begin();
+
+            SupplyInventoryCalc supplyInventoryCalc = new SupplyInventoryCalc();
+            supplyInventoryCalc.processAllUserFlaggedUserListSupplyInventory();
+            tsDataSource.commit();
+            tsDataSource.begin();
+
+            DemandPriorityCalc demandPriorityCalc = new DemandPriorityCalc();
+            demandPriorityCalc.processAllUserFlaggedUserListDemandPriority();
+            tsDataSource.commit();
+            tsDataSource.begin();
+
+            UserTopicCalc userTopicCalc = new UserTopicCalc();
+            userTopicCalc.processAllUserFlaggedUserListUserTopic();
+            tsDataSource.commit();
+            tsDataSource.begin();
+            UserUserCalc userUserCalc = new UserUserCalc();
+            userUserCalc.processAllUserFlaggedUserListUserUser();
+            tsDataSource.commit();
+        } catch (TasteSyncException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            if (tsDataSource != null) {
+                try {
+                    tsDataSource.rollback();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        } finally {
+            if (tsDataSource != null) {
+                tsDataSource.close();
+                tsDataSource.closeConnection(connection, null, null);
+            }
+        }
+    }
+}
