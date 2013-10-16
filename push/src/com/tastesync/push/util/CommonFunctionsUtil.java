d994613fccd376428a987011f7bf4b119871a11f
dusseltal <jagmohan.khulbe@gmail.com>
2013-10-16 23:40:43 +0200

    use jar, delete

--- push/src/com/tastesync/push/util/CommonFunctionsUtil.java
+++
@@ -1,9 +0,0 @@
-package com.tastesync.push.util;
-
-public class CommonFunctionsUtil {
-    public static String getModifiedValueString(String inputString) {
-        return ((inputString == null) ? "" : inputString.trim());
-
-        //return inputString;
-    }
-}
