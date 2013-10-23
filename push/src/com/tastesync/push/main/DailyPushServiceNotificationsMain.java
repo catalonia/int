2087a44ce4de8a3a48041fd749ad93973767ada2
dusseltal <jagmohan.khulbe@gmail.com>
2013-10-23 22:58:15 +0200

    between 11/23 pm

--- push/src/com/tastesync/push/main/DailyPushServiceNotificationsMain.java
+++ push/src/com/tastesync/push/main/DailyPushServiceNotificationsMain.java
@@ -1,12 +1,11 @@
 package com.tastesync.push.main;
 
-import com.tastesync.common.utils.CommonFunctionsUtil;
-
 import com.tastesync.push.exception.TasteSyncException;
 import com.tastesync.push.services.PushService;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 
 public class DailyPushServiceNotificationsMain {
@@ -17,12 +16,16 @@ public class DailyPushServiceNotificationsMain {
         PushService pushService = new PushService();
 
         try {
-            Date currentDateTime = CommonFunctionsUtil.convertJodaTimezoneFromCurrentTimezoneToEST(new Date());
-            // between 11 AM and 23 PM
-            if ((currentDateTime.getHours() >= 11) &&
-                    (currentDateTime.getHours() <= 23)) {
+            Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("US/Eastern"));
+            calendar.setTime(new Date());
+        	//System.out.println("Executing PUSH notifications "+calendar);
+            // between 11 (AM) and 23 (11 PM)
+            if ((calendar.get(Calendar.HOUR_OF_DAY) >= 11) &&
+                    (calendar.get(Calendar.HOUR_OF_DAY) <= 23)) {
+            	System.out.println("Executing PUSH notifications "+calendar);
                 pushService.dailyPushServiceNotifications();
             }
+
         } catch (TasteSyncException e) {
             e.printStackTrace();
         }
