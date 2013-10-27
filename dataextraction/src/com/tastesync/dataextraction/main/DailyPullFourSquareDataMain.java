c46e98da448aebd528df8560d3522936f9c3544a
dusseltal <jagmohan.khulbe@gmail.com>
2013-10-27 04:53:22 +0100

    timing for performing the request

--- dataextraction/src/com/tastesync/dataextraction/main/DailyPullFourSquareDataMain.java
+++ dataextraction/src/com/tastesync/dataextraction/main/DailyPullFourSquareDataMain.java
@@ -26,47 +26,69 @@ public class DailyPullFourSquareDataMain {
     public static void main(String[] args) {
         Date currentDate = new Date();
 
-        //check day 
-        boolean runProcessToday = isProcessToBeRunToday(currentDate);
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(
-                    "US/Eastern"));
-        calendar.setTime(currentDate);
-        System.out.println("Before Executing Daily Pull FourSquare Data "+calendar);
-        if (!runProcessToday) {
-            // further logics to wait
-            System.out.println("hours=" +
-                (24 - calendar.get(Calendar.HOUR_OF_DAY)) + "SLEEP for " +
-                (3600000 * (24 - calendar.get(Calendar.HOUR_OF_DAY))));
-            Thread.currentThread();
+        while (true) {
+            //check day 
+            boolean runProcessToday = isProcessToBeRunToday(currentDate);
+            Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(
+                        "US/Eastern"));
+            calendar.setTime(currentDate);
+            System.out.println("Before Executing Daily Pull FourSquare Data " +
+                calendar);
 
-            try {
-                Thread.sleep(3600000 * (24 -
-                    calendar.get(Calendar.HOUR_OF_DAY)));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+            if (!runProcessToday) {
+                // further logics to wait
+                System.out.println("hours=" +
+                    (24 - calendar.get(Calendar.HOUR_OF_DAY)) + "SLEEP for " +
+                    (3600000 * (24 - calendar.get(Calendar.HOUR_OF_DAY))));
+                Thread.currentThread();
 
-        boolean runProcessNow = false;
-
-        if (runProcessToday) {
-            //run between 1 and 8 in the night!
-            if ((calendar.get(Calendar.HOUR_OF_DAY) >= 1) &&
-                    (calendar.get(Calendar.HOUR_OF_DAY) <= 8)) {
-                runProcessNow = true;
+                try {
+                    Thread.sleep(3600000 * (24 -
+                        calendar.get(Calendar.HOUR_OF_DAY)));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
             }
 
-            if (!runProcessNow) {
-                if (calendar.get(Calendar.HOUR_OF_DAY) > 8) {
+            boolean runProcessNow = false;
+
+            if (runProcessToday) {
+                //run between 1 and 8 in the night!
+                if ((calendar.get(Calendar.HOUR_OF_DAY) >= 1) &&
+                        (calendar.get(Calendar.HOUR_OF_DAY) <= 8)) {
+                    runProcessNow = true;
+                }
+
+                if (!runProcessNow) {
+                    if (calendar.get(Calendar.HOUR_OF_DAY) > 8) {
+                        // further logics to wait
+                        System.out.println("hours=" +
+                            (1+24 - calendar.get(Calendar.HOUR_OF_DAY)) +
+                            "SLEEP for " +
+                            (3600000 * (1+24 -
+                            calendar.get(Calendar.HOUR_OF_DAY))));
+                        Thread.currentThread();
+
+                        try {
+                            Thread.sleep(3600000 * (1+24 -
+                                calendar.get(Calendar.HOUR_OF_DAY)));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                if (calendar.get(Calendar.HOUR_OF_DAY) < 1) {
                     // further logics to wait
                     System.out.println("hours=" +
-                        (24 - calendar.get(Calendar.HOUR_OF_DAY)) +
+                        (1 - calendar.get(Calendar.HOUR_OF_DAY)) +
                         "SLEEP for " +
-                        (3600000 * (24 - calendar.get(Calendar.HOUR_OF_DAY))));
+                        (3600000 * (1 - calendar.get(Calendar.HOUR_OF_DAY))));
                     Thread.currentThread();
 
                     try {
-                        Thread.sleep(3600000 * (24 -
+                        Thread.sleep(3600000 * (1 -
                             calendar.get(Calendar.HOUR_OF_DAY)));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -74,114 +96,101 @@ public class DailyPullFourSquareDataMain {
                 }
             }
 
-            if (calendar.get(Calendar.HOUR_OF_DAY) < 1) {
-                // further logics to wait
-                System.out.println("hours=" +
-                    (1 - calendar.get(Calendar.HOUR_OF_DAY)) + "SLEEP for " +
-                    (3600000 * (1 - calendar.get(Calendar.HOUR_OF_DAY))));
-                Thread.currentThread();
+            System.out.println("Executing Daily Pull FourSquare Data " +
+                calendar);
 
-                try {
-                    Thread.sleep(3600000 * (1 -
-                        calendar.get(Calendar.HOUR_OF_DAY)));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        System.out.println("Executing Daily Pull FourSquare Data "+calendar);
-        
-        DefaultIOHandler defaultIOHandler = new DefaultIOHandler();
-        FoursquareApi foursquareApi = new FoursquareApi(TSConstants.CLIENT_ID,
-                TSConstants.CLIENT_SECRET, TSConstants.REDIRECURI,
-                defaultIOHandler);
+            DefaultIOHandler defaultIOHandler = new DefaultIOHandler();
+            FoursquareApi foursquareApi = new FoursquareApi(TSConstants.CLIENT_ID,
+                    TSConstants.CLIENT_SECRET, TSConstants.REDIRECURI,
+                    defaultIOHandler);
 
-        //create input files Daily_SortedData_FactualId
-        System.out.println("************ Start ************");
+            //create input files Daily_SortedData_FactualId
+            System.out.println("************ Start ************");
 
-        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-        Date now = new Date();
-        String strNowDate = sdfDate.format(now);
+            SimpleDateFormat sdfDate = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+            Date now = new Date();
+            String strNowDate = sdfDate.format(now);
 
-        WriteFactualIdDataInfo writeFactualIdDataInfo = new WriteFactualIdDataInfo();
-        String outputFilePath = "./outputDir/" + strNowDate +
-            "_daily_outputFile_sortedData_FactualId.txt";
+            WriteFactualIdDataInfo writeFactualIdDataInfo = new WriteFactualIdDataInfo();
+            String outputFilePath = "./outputDir/" + strNowDate +
+                "_daily_outputFile_sortedData_FactualId.txt";
 
-        List<FactualDataVO> factualIdList;
-        DailyRestaurantFactual4SqData dailyRestaurantFactual4SqData = new DailyRestaurantFactual4SqData();
-
-        try {
-            int pullEligInd = 2;
-            int beforeNDays = 28;
-            int lastMatchInd = 1;
-            dailyRestaurantFactual4SqData.identifyRestaurantsFourSqExtractUsingUpdate(pullEligInd,
-                beforeNDays, lastMatchInd);
-
-            pullEligInd = 3;
-
-            int lastUpdatedNDays = 28;
-
-            lastMatchInd = 1;
-
-            int accessNDays = 30;
-            dailyRestaurantFactual4SqData.identifyUserAccessRestaurantAttempted(pullEligInd,
-                lastUpdatedNDays, lastMatchInd, accessNDays);
-
-            pullEligInd = 1;
-            beforeNDays = 42;
-            lastMatchInd = 0;
-
-            dailyRestaurantFactual4SqData.identifyRestaurantsFourSqExtractUsingUpdate(pullEligInd,
-                beforeNDays, lastMatchInd);
-
-            pullEligInd = 1;
-            lastUpdatedNDays = 28;
-
-            lastMatchInd = 1;
-            accessNDays = 30;
-            dailyRestaurantFactual4SqData.identifyUserAccessRestaurantAttempted(pullEligInd,
-                lastUpdatedNDays, lastMatchInd, accessNDays);
-
-            factualIdList = dailyRestaurantFactual4SqData.getFactualDataVOListsForExtraction();
-            writeFactualIdDataInfo.writeFactualDataToFile(outputFilePath,
-                factualIdList);
-
-            // check that input file is available.
-            // if not, then use WriteFactualIdDataInfoMain to create the file 
-            // and copy in the config directorz
-
-            //String inputFactualIdDataFile = "./config/Daily_SortedData_FactualId.txt";
-            String inputFactualIdDataFile = outputFilePath;
-            String outputSqlFilePath = "./outputDir/" + strNowDate +
-                "_daily_Factual_4SQ_outputFile.sql";
-            String outputInvalidFactualDataFile = "./outputDir/" + strNowDate +
-                "_daily_INVALID_Factual_4SQ_outputFile.txt";
-            RestaurantFactual4SqData restaurantFactual4SqData = new RestaurantFactual4SqData();
-
-            restaurantFactual4SqData.processRestaurantFactual4SqData(TSConstants.DATAEXTRACTION_SOURCETYPE.FOURSQUARE,
-                TSConstants.CLIENT_ID, TSConstants.CLIENT_SECRET,
-                TSConstants.REDIRECURI, defaultIOHandler, foursquareApi,
-                inputFactualIdDataFile, outputSqlFilePath,
-                outputInvalidFactualDataFile);
-
-            // process the output file via script or run in SQL!!!
-            String oExecResult = null;
+            List<FactualDataVO> factualIdList;
+            DailyRestaurantFactual4SqData dailyRestaurantFactual4SqData = new DailyRestaurantFactual4SqData();
 
             try {
-                oExecResult = CommonFunctionsUtil.exec(TSConstants.DAILY_PULL_4SQ_SCRIPT +
-                        " " + outputSqlFilePath);
-            } catch (com.tastesync.common.exception.TasteSyncException e) {
+                int pullEligInd = 2;
+                int beforeNDays = 28;
+                int lastMatchInd = 1;
+                dailyRestaurantFactual4SqData.identifyRestaurantsFourSqExtractUsingUpdate(pullEligInd,
+                    beforeNDays, lastMatchInd);
+
+                pullEligInd = 3;
+
+                int lastUpdatedNDays = 28;
+
+                lastMatchInd = 1;
+
+                int accessNDays = 30;
+                dailyRestaurantFactual4SqData.identifyUserAccessRestaurantAttempted(pullEligInd,
+                    lastUpdatedNDays, lastMatchInd, accessNDays);
+
+                pullEligInd = 1;
+                beforeNDays = 42;
+                lastMatchInd = 0;
+
+                dailyRestaurantFactual4SqData.identifyRestaurantsFourSqExtractUsingUpdate(pullEligInd,
+                    beforeNDays, lastMatchInd);
+
+                pullEligInd = 1;
+                lastUpdatedNDays = 28;
+
+                lastMatchInd = 1;
+                accessNDays = 30;
+                dailyRestaurantFactual4SqData.identifyUserAccessRestaurantAttempted(pullEligInd,
+                    lastUpdatedNDays, lastMatchInd, accessNDays);
+
+                factualIdList = dailyRestaurantFactual4SqData.getFactualDataVOListsForExtraction();
+                writeFactualIdDataInfo.writeFactualDataToFile(outputFilePath,
+                    factualIdList);
+
+                // check that input file is available.
+                // if not, then use WriteFactualIdDataInfoMain to create the file 
+                // and copy in the config directorz
+
+                //String inputFactualIdDataFile = "./config/Daily_SortedData_FactualId.txt";
+                String inputFactualIdDataFile = outputFilePath;
+                String outputSqlFilePath = "./outputDir/" + strNowDate +
+                    "_daily_Factual_4SQ_outputFile.sql";
+                String outputInvalidFactualDataFile = "./outputDir/" +
+                    strNowDate + "_daily_INVALID_Factual_4SQ_outputFile.txt";
+                RestaurantFactual4SqData restaurantFactual4SqData = new RestaurantFactual4SqData();
+
+                restaurantFactual4SqData.processRestaurantFactual4SqData(TSConstants.DATAEXTRACTION_SOURCETYPE.FOURSQUARE,
+                    TSConstants.CLIENT_ID, TSConstants.CLIENT_SECRET,
+                    TSConstants.REDIRECURI, defaultIOHandler, foursquareApi,
+                    inputFactualIdDataFile, outputSqlFilePath,
+                    outputInvalidFactualDataFile);
+
+                // process the output file via script or run in SQL!!!
+                String oExecResult = null;
+
+                try {
+                    oExecResult = CommonFunctionsUtil.exec(TSConstants.DAILY_PULL_4SQ_SCRIPT +
+                            " " + outputSqlFilePath);
+                } catch (com.tastesync.common.exception.TasteSyncException e) {
+                    e.printStackTrace();
+                }
+
+                if (oExecResult.contains("ERROR:")) {
+                    //TODO rename the output sql file
+                }
+            } catch (TasteSyncException e) {
                 e.printStackTrace();
             }
 
-            if (oExecResult.contains("ERROR:")) {
-                //TODO rename the output sql file
-            }
-        } catch (TasteSyncException e) {
-            e.printStackTrace();
+            System.out.println("************ End ************");
         }
-
-        System.out.println("************ End ************");
     }
 
     private static boolean isProcessToBeRunToday(Date currentDate) {
