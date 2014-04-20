5f82f22309032ae3d45ff8369c4be35cc5228704
dusseltal <jagmohan.khulbe@gmail.com>
2014-04-21 01:47:28 +0200

    Apr. 2014 updates

--- algo/src/com/tastesync/algo/util/TSConstants.java
+++ algo/src/com/tastesync/algo/util/TSConstants.java
@@ -5,7 +5,7 @@ public interface TSConstants {
     public static final String EMPTY_STRING = "";
     public static final String[] EMPTY_STRING_ARRAY = new String[] {  };
     public static final int MAX_RECOREQUEST_CONSIDERED = 10;
-    public static final int PAGINATION_GAP=10;
+    public static final int PAGINATION_GAP=50;
     public static final int SLEEP_TIME_BETWEEN_TWO_REQUEST_ITERATION_IN_MILLISECONDS=600000;
     public static final int DEMAND_TIER3_START_SLEEP_TIME=300000;
     public static final int PI_ASSIGNED_START_SLEEP_TIME=60000;
